2645ac07f6b57c2688687efa6131d7deb2bf46c8
usemeastrashcan <ahmadhasnat490@gmail.com>
2024-07-26 23:06:54 +0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://shapparz.vercel.app",
+    origin: ["https://shapparz.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from './context/Auth';
 import { CartProvider } from './context/Cart.jsx';
+
+axios.defaults.withCredentials = true;
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
